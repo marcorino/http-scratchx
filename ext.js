acb2fb2eecd7ec90b643d5407b3ee7fd710a0168
marcorino <marcolino@marco-mac.local>
2016-10-30 21:24:49 +0100

    .

--- ext.js
+++ ext.js
@@ -21,39 +21,65 @@
     //         callback();
     //     }, wait*1000);
     // };
+
+    var createCORSRequest = function(method, url) {
+        var xhr = new XMLHttpRequest();
+        if ("withCredentials" in xhr) {
+            // Check if the XMLHttpRequest object has a "withCredentials" property.
+            // "withCredentials" only exists on XMLHTTPRequest2 objects.
+            xhr.open(method, url, true);
+        } else if (typeof XDomainRequest != "undefined") {
+            // Otherwise, check if XDomainRequest.
+            // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+            xhr = new XDomainRequest();
+            xhr.open(method, url);
+        } else {
+            // Otherwise, CORS is not supported by the browser.
+            xhr = null;
+        }
+        return xhr;
+    }
+
     var output = '';
     var url_beginning = 'https://www.eventbriteapi.com/v3/events/20789736662?token=WFGW7FS5KX72BLBLZUXN';
 
     ext.make_call_temp = function(callback) {
-        // var url = 'https://www.eventbriteapi.com/v3/events/20789736662?token=WFGW7FS5KX72BLBLZUXN';
-        // $.ajax({
-        //       url: url_beginning,
-        //       method: 'GET',
-        //       dataType: 'jsonp',
-        //       success: function(data) {
-        //           //name = data['name']['text'];
-        //           console.log(data);
-        //           callback('name');
-        //       }
-        // });
-
-        var fullNameRequest = new XMLHttpRequest();
+        // var fullNameRequest = new XMLHttpRequest();
         
-        fullNameRequest.onreadystatechange = function() {
-            if (fullNameRequest.readyState === XMLHttpRequest.DONE) {
-                var fullNameText = fullNameRequest.responseText;
-                try {
-                    fullNameText = JSON.Parse(fullNameText);
-                    output = fullNameText.name.text;
-                    callback(output);
-                    output = '';
-                } catch (e) {
-                    callback('error');
-                }
-            }
+        // fullNameRequest.onreadystatechange = function() {
+        //     if (fullNameRequest.readyState === XMLHttpRequest.DONE) {
+        //         var fullNameText = fullNameRequest.responseText;
+        //         try {
+        //             fullNameText = JSON.Parse(fullNameText);
+        //             output = fullNameText.name.text;
+        //             callback(output);
+        //             output = '';
+        //         } catch (e) {
+        //             callback('error');
+        //         }
+        //     }
+        // }
+
+        xhr.onload = function() {
+            var responseText = xhr.responseText;
+            console.log(responseText);
+            callback(responseText);
+            // process the response.
+        };
+
+        xhr.onerror = function() {
+            console.log('There was an error!');
+            callback('There was an error!');
+        };
+
+
+        var xhr = createCORSRequest('GET', url_beginning);
+        if (!xhr) {
+            throw new Error('CORS not supported');
         }
-        fullNameRequest.open("GET", url_beginning);
-        fullNameRequest.send();
+
+        // fullNameRequest.open("GET", url_beginning);
+        // fullNameRequest.send();
     }
 
     // Block and block menu descriptions
